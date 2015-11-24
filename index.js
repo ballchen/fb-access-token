aaad4102068f7045047239a9cfbf013b7c1ee37a
ballchen <kerkerball@gmail.com>
2015-11-24 16:56:27 +0800

    parse login-form

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var request = require('request')
+var cheerio = require('cheerio')
+var j = request.jar();
 request = request.defaults({
-  jar: true
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0',
+    'Cookie': '_js_reg_fb_gate=https%3A%2F%2Fwww.facebook.com%2F;',
+    'referer': 'https://www.messenger.com/'
+  },
+  jar: j
 })
 
 var access_token = function (username, password, appId) {
@@ -10,37 +17,38 @@ var access_token = function (username, password, appId) {
 }
 
 access_token.prototype.login = function (callback) {
-  // login to facebook
-  request.post({
-    url: 'https://www.facebook.com/login.php',
-    headers: {
-      cookie: '_js_datr=o-yfVdmCImbo0AiTO9ir7YVl; _js_reg_fb_ref=https%3A%2F%2Fwww.facebook.com%2F; _js_reg_fb_gate=https%3A%2F%2Fwww.facebook.com%2F; dpr=1.100000023841858',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.132 Safari/537.36'
-    },
-    form: {
-      lsd: 'AVpVYcLw',
-      email: this.username,
-      pass: this.password,
-      persistent: '1',
-      default_persistent: '0',
-      timezone: '-480',
-      lgndim: 'eyJ3IjoxOTIwLCJoIjoxMDgwLCJhdyI6MTkyMCwiYWgiOjEwNTcsImMiOjI0fQ==',
-      lgnrnd: '090243_3H_E',
-      lgnjs: '1436544166',
-      locale: 'zh_TW',
-      qsstamp: 'W1tbMTMsMjcsNDUsNTgsNjksNzAsNzcsNzgsODEsODksMTAxLDEwNSwxMjEsMTI0LDEyOSwxMzAsMTQ5LDE2OSwyNjAsMjg3LDI5MywyOTgsMzE0LDMyNywzNTEsMzUyLDM2NywzNzcsMzc4LDM5Niw0NjMsNDY0LDQ4MCw1MjcsNTUxLDU2NCw2MDgsNjIwLDYyOSw2NjQsNzYwLDc3Ml1dLCJBWmt4bUNiWDkyZ3lFQkpWNGcwRmxtdE10Zm43cGhoS3M0T1cyVUExR1N1Mm1GdUFaZ2hpUHAwalV1aHNaTVRFS3ZOeWg1UkVBTERoazRtZXZhSUg3SG9oQXlIZ2dmSnc5dG5IY3hqOG00VUhGclBHQTdaRk5GYlZMOGh4bVZhRjZXVGkwVVZoM1FTOWhJQkVKeGJlbmppMDhLWWRET2NfaUl0blRxbzdJSDhiQ1pfeXI5ZEhUYzJsOFdlOWxNeUlBcE0yaUd4Z0I0X1A5YndTTldyOUVoU0pySmFzX0F3Tkx6TkZ2UWV3M183Vm9Mb3h6NThBU0wyUE9vUVRTVHpuMGNxS2pTbng0dFZ4dUxsLUxMWXJEamNjIl0='
-    }
-  }, function (err, res, body) {
-    if (err) {
-      return callback(err)
-    }
 
-    callback(null)
+  var that = this;
+  // login to facebook
+   
+  request.get('https://www.facebook.com/login.php', function(err, httpResponse, body){
+    $ = cheerio.load(body);
+    var login_form = new Object();
+    $('form#login_form input').each(function(i, elem) {
+      login_form[$(this).attr('name')] = $(this).attr('value')
+    })
+
+    login_form.pass = that.password
+    login_form.email = that.username
+    
+    request({
+      method: 'POST',
+      url: 'https://www.facebook.com/login.php',
+      form: login_form
+    }, function (err, res, body) {
+      if (err) {
+        return callback(err)
+      }
+
+      callback(null)
+    })
   })
+  
 }
 
 access_token.prototype.getToken = function (callback) {
   // use graph api explorer to get access token
+  console.log('https://developers.facebook.com/tools/explorer/' + this.appId + '/permissions?version=v2.1&__a=1&__dyn=5U463-i3S2e4oK4pomXWo5O12wAxu&__req=2&__rev=1470714');
   request.get({
     url: 'https://developers.facebook.com/tools/explorer/' + this.appId + '/permissions?version=v2.1&__a=1&__dyn=5U463-i3S2e4oK4pomXWo5O12wAxu&__req=2&__rev=1470714'
   }, function (err, res, body) {
@@ -53,6 +61,7 @@ access_token.prototype.getToken = function (callback) {
     }
 
     try {
+      // console.log(body);
       body = JSON.parse(body.replace('for (;;);', ''))
     } catch (e) {
       return callback(new Error('JSON parse error:' + e))
